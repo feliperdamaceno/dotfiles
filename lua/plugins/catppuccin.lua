c8ad00853dabeae8de3f035c22b4893a13b3ba05
feliperdamaceno <feliperdamaceno@sent.com>
2024-09-28 15:20:45 +0100

    feat: include catppuccin theme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      local config = require("catppuccin")
+      
+      config.setup({
+        no_italic = true,
+        no_bold = true,
+        no_underline = true,
+      })
+
+      vim.cmd.colorscheme "catppuccin-frappe"
+    end
+  }
+}
