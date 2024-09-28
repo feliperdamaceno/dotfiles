6fe7802a93504d8a424c40a5dbc539149143b256
feliperdamaceno <feliperdamaceno@sent.com>
2024-09-28 18:13:48 +0100

    feat: include lsp server plugin

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      local config = require("mason-lspconfig")
+      config.setup({
+        automatic_installation = true
+      })
+
+      local lspconfig = require("lspconfig")
+      local lsp_defaults = lspconfig.util.default_config
+
+      lsp_defaults.capabilities = vim.tbl_deep_extend(
+        'force',
+        lsp_defaults.capabilities,
+        require('cmp_nvim_lsp').default_capabilities()
+      )
+
+      lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.html.setup({})
+      lspconfig.tailwindcss.setup({})
+      lspconfig.gopls.setup({})
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
