661cdd94c3832078c066670771894895f8d75e55
feliperdamaceno <feliperdamaceno@sent.com>
2024-09-29 01:06:27 +0100

    refactor: swap neo-tree for nvim-tree

---
+++ lua/plugins/nvimtree.lua
@@ -0,0 +1,47 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local config = require("nvim-tree")
+
+    config.setup({
+      filters = { dotfiles = false },
+      disable_netrw = true,
+      hijack_cursor = true,
+      sync_root_with_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = false,
+      },
+      view = {
+        width = 35,
+        preserve_window_proportions = true,
+      },
+      renderer = {
+        root_folder_label = false,
+        highlight_git = true,
+        icons = {
+          glyphs = {
+            default = "󰈚",
+            folder = {
+              default = "",
+              empty = "",
+              empty_open = "",
+              open = "",
+              symlink = "",
+            },
+            git = { unmerged = "" },
+          },
+        },
+      },
+    })
+
+    vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
+    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>")
+  end,
+}
